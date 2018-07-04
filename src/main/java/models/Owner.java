aca47c296fa240eadc04fb0d2f168787ecb74f3c
Cleyrauz <cleyra.uzcategui@gmail.com>
2018-07-04 21:14:58 +0100

    Added owner class and OneToMany relationship

---
+++ src/main/java/models/Owner.java
@@ -0,0 +1,52 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name ="owners")
+
+public class Owner {
+
+    private String name;
+    private int id;
+    private Game favouriteGame;
+
+    public Owner(){
+
+    }
+
+    public Owner(String name, Game favouriteGame) {
+        this.name = name;
+        this.favouriteGame = favouriteGame;
+    }
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Column(name="name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @OneToMany(mappedBy = "owner", fetch = FetchType.LAZY)
+    public Game getFavouriteGame() {
+        return favouriteGame;
+    }
+
+    public void setFavouriteGame(Game favouriteGame) {
+        this.favouriteGame = favouriteGame;
+    }
+}
